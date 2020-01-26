d54e9f3fbd9056ceb053965d3f1ff3b2dc7a663e
nouha <nahouta2006@hotmail.fr>
2020-01-26 15:00:32 +0100

    Updates

---
+++ precache-manifest.2f61ab48ddf28fffa94e431527701abf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bdce8b42ec25fdd9c66f3ffda969175",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c5614a1f5f034cd680f2",
+    "url": "/portfolio/static/css/main.54ed6840.chunk.css"
+  },
+  {
+    "revision": "5330fa544aa2a97d3e9c",
+    "url": "/portfolio/static/js/2.10a5f204.chunk.js"
+  },
+  {
+    "revision": "c5614a1f5f034cd680f2",
+    "url": "/portfolio/static/js/main.7f33eaa8.chunk.js"
+  },
+  {
+    "revision": "76aa8cbd67b5c937ec2c",
+    "url": "/portfolio/static/js/runtime-main.e92c632a.js"
+  },
+  {
+    "revision": "84f12002da290c7fe67dd999896ae0d5",
+    "url": "/portfolio/static/media/life.84f12002.jpg"
+  },
+  {
+    "revision": "053c908dbff42f11a6959f5b1740a458",
+    "url": "/portfolio/static/media/nouha-logo.053c908d.png"
+  }
+]);
